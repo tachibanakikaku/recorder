0f910fae8ab4b8df906c1e941299c44368c6a522
mryoshio <yoshiokaas@gmail.com>
2015-12-05 22:56:45 +0900

    show times in HomeView
    - close #5

--- recorder/HomeView.swift
+++ recorder/HomeView.swift
@@ -10,9 +10,79 @@ import Foundation
 import UIKit
 
 class HomeView: UIView {
+
+    lazy var lastRangedAtLabel: UILabel = UILabel()
+    lazy var lastRangedAtText: UITextField = UITextField()
+    lazy var lastSentAtLabel: UILabel = UILabel()
+    lazy var lastSentAtText: UITextField = UITextField()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = ColorUtil.baseColor()
+
+        addLastRangedAt()
+        addLastSentAt()
+        addConstraints()
+    }
+
+    private func addLastRangedAt() {
+        lastRangedAtLabel.textAlignment = .Right
+        lastRangedAtLabel.text = NSLocalizedString("view.property.lastRangedAt", comment: "lastRangedAt label")
+        lastRangedAtLabel.translatesAutoresizingMaskIntoConstraints = false
+        lastRangedAtLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+
+        lastRangedAtText.textAlignment = .Left
+        lastRangedAtText.translatesAutoresizingMaskIntoConstraints = false
+        lastRangedAtText.font = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle2)
+
+        addSubview(lastRangedAtLabel)
+        addSubview(lastRangedAtText)
+    }
+
+    private func addLastSentAt() {
+        lastSentAtLabel.textAlignment = .Right
+        lastSentAtLabel.text = NSLocalizedString("view.property.lastSentAtLabel", comment: "lastSentAtLabel label")
+        lastSentAtLabel.translatesAutoresizingMaskIntoConstraints = false
+        lastSentAtLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+
+        lastSentAtText.textAlignment = .Left
+        lastSentAtText.translatesAutoresizingMaskIntoConstraints = false
+        lastSentAtText.font = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle2)
+
+        addSubview(lastSentAtLabel)
+        addSubview(lastSentAtText)
+    }
+
+    private func addConstraints() {
+        let views = [
+            "lastRangedAtLabel":lastRangedAtLabel,
+            "lastRangedAtText":lastRangedAtText,
+            "lastSentAtLabel":lastSentAtLabel,
+            "lastSentAtText":lastSentAtText
+        ]
+
+        let metrics = [
+            "h_padding":30.0,
+            "v_padding_h":80.0,
+            "v_padding":20.0,
+            "v_item_padding":50.0
+        ]
+
+        let hLastRangedAtLabel = NSLayoutConstraint.constraintsWithVisualFormat("|-h_padding-[lastRangedAtLabel]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
+
+        let hLastRangedAtText = NSLayoutConstraint.constraintsWithVisualFormat("|-h_padding-[lastRangedAtText]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
+
+        let hLastSentAtLabel = NSLayoutConstraint.constraintsWithVisualFormat("|-h_padding-[lastSentAtLabel]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
+
+        let hLastSentAtText = NSLayoutConstraint.constraintsWithVisualFormat("|-h_padding-[lastSentAtText]", options: NSLayoutFormatOptions(rawValue: 0), metrics: metrics, views: views)
+
+        let vLabel = NSLayoutConstraint.constraintsWithVisualFormat("V:|-v_padding_h-[lastRangedAtLabel]-v_padding-[lastRangedAtText]-v_item_padding-[lastSentAtLabel]-v_padding-[lastSentAtText]", options: NSLayoutFormatOptions.AlignAllLeft, metrics: metrics, views: views)
+
+        addConstraints(hLastRangedAtLabel)
+        addConstraints(hLastRangedAtText)
+        addConstraints(hLastSentAtLabel)
+        addConstraints(hLastSentAtText)
+        addConstraints(vLabel)
     }
 
     required init?(coder aDecoder: NSCoder) {
--- recorder/HomeViewController.swift
+++ recorder/HomeViewController.swift
@@ -15,13 +15,18 @@ class HomeViewController: UIViewController, ESTBeaconManagerDelegate {
 
     lazy var major: Int = Int(ConstantUtil.ud.valueForKey(ConstantUtil.major) as! String)!
     lazy var minor: Int = Int(ConstantUtil.ud.valueForKey(ConstantUtil.minor) as! String)!
+    let dateFormatter = NSDateFormatter()
 
     lazy var rangedBeacons = [String: [NSDate]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(HomeView(frame: self.view.frame))
-        
+        self.view = HomeView(frame: self.view.frame)
+
+        let locale = NSLocale.currentLocale().objectForKey(NSLocaleIdentifier) as! String
+        dateFormatter.locale = NSLocale(localeIdentifier: locale)
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+
         self.beaconManager.delegate = self
         self.beaconManager.requestAlwaysAuthorization()
     }
@@ -29,15 +34,23 @@ class HomeViewController: UIViewController, ESTBeaconManagerDelegate {
     func beaconManager(manager: AnyObject, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
         if let nearestBeacon = beacons.first {
             if nearestBeacon.major == major && nearestBeacon.minor == minor {
+                let now = NSDate()
                 let key = "\(major):\(minor)"
+
+                (self.view as! HomeView).lastRangedAtText.text = dateFormatter.stringFromDate(now)
+
                 if rangedBeacons.indexForKey(key) == nil {
                     rangedBeacons.updateValue([NSDate()], forKey: key)
                     print("first: \(rangedBeacons)")
                 } else {
-                    let now: NSDate = NSDate()
                     let past = rangedBeacons[key]!.last!
+
                     if now.timeIntervalSinceDate(past) > DateTimeUtil.record_interval_seconds {
-                        sendToServer(key, datetime: now)
+                        if sendToServer(key, datetime: now) {
+                            // TODO: clear Array, rangedBeacons.indexForKey(key)
+
+                            (self.view as! HomeView).lastSentAtText.text = dateFormatter.stringFromDate(now)
+                        }
                         rangedBeacons[key]?.append(now)
                         print("\(rangedBeacons[key]!.count): \(rangedBeacons)")
                     }
@@ -46,8 +59,9 @@ class HomeViewController: UIViewController, ESTBeaconManagerDelegate {
         }
     }
 
-    private func sendToServer(key: String, datetime: NSDate) {
-        // clear Array: rangedBeacons.indexForKey(key) if succeed in send to server
+    private func sendToServer(key: String, datetime: NSDate) -> Bool {
+        // send to server
+        return true
     }
 
     override func didReceiveMemoryWarning() {
